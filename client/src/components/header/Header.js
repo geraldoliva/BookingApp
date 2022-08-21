937398cba73b806df7fd60b0ae26b5429677a0a3
Gerald Oliva <geraldsamoliva@gmail.com>
2022-08-21 14:56:37 -0400

    Added AuthContext

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -16,6 +16,7 @@ import "react-date-range/dist/theme/default.css"; // theme css file
 import "./Header.css";
 import { useNavigate } from "react-router-dom";
 import { SearchContext } from "../../context/SearchContext";
+import { AuthContext } from "../../context/AuthContext";
 
 const Header = ({ type }) => {
   const [destination, setDestination] = useState("");
@@ -35,6 +36,7 @@ const Header = ({ type }) => {
   });
 
   const navigate = useNavigate();
+  const { user } = useContext(AuthContext);
 
   const handleOption = (name, operation) => {
     setOptions((prev) => {
@@ -90,7 +92,7 @@ const Header = ({ type }) => {
               Get rewarded for your travels - unlock instant savings of 10% or
               more with a free GOBooking account
             </p>
-            <button className="headerBtn">Sign in / Register</button>
+            {!user && <button className="headerBtn">Sign in / Register</button>}
             <div className="headerSearch">
               <div className="headerSearchItem">
                 <FontAwesomeIcon icon={faBed} className="headerIcon" />
